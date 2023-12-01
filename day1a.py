23a4b2e8a747d6a005ed26770ad0b7abec092f9f
sotiris59 <152623192+sotiris59@users.noreply.github.com>
2023-12-01 16:44:22 +0200

    Day1
    day 1

---
+++ day1a.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+home_directory = os.path.expanduser("~")
+print("Home Directory:", home_directory)
+
+
+with open("/Users/skousouris/adv2023/day1a.txt") as trebuchet:
+    lines = trebuchet.readlines()
+
+with open("/Users/skousouris/adv2023/temp1.txt" , "w") as temp1:
+    temp1.write("")
+
+
+for line in lines[:]:
+    a = ''.join(char for char in line if char.isdigit())
+    with open("/Users/skousouris/adv2023/temp1.txt", "a") as temp1:
+        temp1.write(a+"\n")
+
+with open("/Users/skousouris/adv2023/temp1.txt") as temp1:
+    lines = temp1.readlines()
+
+sum = 0
+num = 0
+for line in lines:
+    first = int(line[0])
+    last = int(line[-2])
+    sum += first*10 + last
+    num += 1
+    print(num,first,last,sum)
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ day1b.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Read the file
+with open("/Users/skousouris/adv2023/day1a.txt") as trebuchet:
+    content = trebuchet.read()
+
+#Perform replacement
+content = content.replace("eight","e8t")
+content = content.replace("one","o1e")
+content = content.replace("two","t2o")
+content = content.replace("three","t3e")
+content = content.replace("four","f4r")
+content = content.replace("five","f5e")
+content = content.replace("six","s6x")
+content = content.replace("seven","s7n")
+content = content.replace("nine","n9e")
+
+#Write content with replaced strings
+with open("/Users/skousouris/adv2023/temp1.txt" , "w") as temp1:
+    temp1.write(content)
+
+with open("/Users/skousouris/adv2023/temp2.txt" , "w") as temp2:
+    temp2.write("")
+
+with open("/Users/skousouris/adv2023/temp1.txt","r") as trebuchet:
+    lines = trebuchet.readlines()
+
+#write only numbers
+for line in lines:
+    a = ''.join(char for char in line if char.isdigit())
+    with open("/Users/skousouris/adv2023/temp2.txt", "a") as temp2:
+        temp2.write(a+"\n")
+
+with open("/Users/skousouris/adv2023/temp2.txt") as temp2:
+    lines = temp2.readlines()
+
+summ = sum(int(line[0])*10 + int(line[-2]) for line in lines)
+
+print(summ)
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
